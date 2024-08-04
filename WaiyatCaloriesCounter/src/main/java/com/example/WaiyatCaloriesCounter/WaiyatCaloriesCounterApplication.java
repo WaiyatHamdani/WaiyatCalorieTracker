ea953e45206cfbfd126f039dca875548fba5ad3b
waiyat.hamdani <waiyat.hamdani@fdmgroup.com>
2024-08-04 17:55:12 -0400

    skeleton for now

---
+++ WaiyatCaloriesCounter/src/main/java/com/example/WaiyatCaloriesCounter/WaiyatCaloriesCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.WaiyatCaloriesCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiyatCaloriesCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaiyatCaloriesCounterApplication.class, args);
+	}
+
+}
---
+++ WaiyatCaloriesCounter/src/test/java/com/example/WaiyatCaloriesCounter/WaiyatCaloriesCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WaiyatCaloriesCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaiyatCaloriesCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
